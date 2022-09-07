52f273db1b76e5577d110a06c66af8c99b7d8b1b
bbrombacher <bbrombacher@me.com>
2022-09-07 04:52:55 -0700

    test main. in root

--- main.go
+++ main.go
@@ -1,33 +1,59 @@
 package main
 
 import (
-	"bbrombacher/cryptoautotrader/coinbase"
-	"bbrombacher/cryptoautotrader/transforms"
-	"fmt"
+	"bbrombacher/cryptoautotrader/controllers/currencies"
+	users "bbrombacher/cryptoautotrader/controllers/users"
+	"bbrombacher/cryptoautotrader/storage"
+	"bbrombacher/cryptoautotrader/storage/sql"
+	"context"
+	goSql "database/sql"
 	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
 
-var socketUrl = "wss://ws-feed.pro.coinbase.com"
+var localDB = "postgres://pguser:pgpass@localhost:9001/robot-transact?sslmode=disable"
 
 func main() {
 
-	coinbaseClient := coinbase.New(socketUrl)
+	dbURL := localDB
+	if os.Getenv("ENV") == "server" {
+		dbURL = os.Getenv("DB_URL")
+	}
 
-	feedParams := transforms.BuildStartTickerParams([]string{"ETH-USD"})
-	tickerID, err := coinbaseClient.StartTickerFeed(feedParams)
+	log.Println("DBURL", dbURL)
+
+	// set up db
+	sqldb, err := goSql.Open(
+		"postgres",
+		dbURL,
+	)
 	if err != nil {
-		log.Fatal("failed to start ticker", err.Error())
+		log.Println("error opening sql", err.Error())
+	}
+	db := sqlx.NewDb(sqldb, "postgres")
+	sqlClient, err := sql.NewSQLClient(context.Background(), db)
+	if err != nil {
+		log.Println("could not start sql client: ", err.Error())
+	}
+	storageClient := storage.NewStorageClient(sqlClient)
+
+	// setup controllers
+	userController := users.Controller{
+		StorageClient: storageClient,
 	}
 
-	defer func() {
-		coinbaseClient.CloseTickerFeed(tickerID)
-	}()
-
-	for {
-		msg, err := coinbaseClient.GetTickerMessages(tickerID)
-		if err != nil {
-			log.Fatal("could not get ticker message", err.Error())
-		}
-		fmt.Printf("msg %#v\n", msg)
+	currencyController := currencies.Controller{
+		StorageClient: storageClient,
 	}
+
+	// setup router
+	r := mux.NewRouter()
+	userController.Register(r)
+	currencyController.Register(r)
+	http.ListenAndServe(":8080", r)
 }
