0d4184d2ac654c4e935d91190dec90c8345e626e
bbrombacher <bbrombacher@me.com>
2022-10-16 14:48:52 -0700

    create buy & sell functions

--- storage/transactions.go
+++ storage/transactions.go
@@ -7,7 +7,8 @@ import (
 	"fmt"
 )
 
-func (s *StorageClient) CreateTransaction(ctx context.Context, tradeSessionID string, input models.TransactionEntry) (*models.TransactionEntry, error) {
+// buy crypto with usd
+func (s *StorageClient) buy(ctx context.Context, tradeSessionID string, input models.TransactionEntry) (*models.TransactionEntry, error) {
 
 	cost := input.Price.Mul(input.Amount)
 
@@ -57,6 +58,75 @@ func (s *StorageClient) CreateTransaction(ctx context.Context, tradeSessionID st
 	}
 
 	return entry, nil
+}
+
+// sell crypto with usd
+func (s *StorageClient) sell(ctx context.Context, tradeSessionID string, input models.TransactionEntry) (*models.TransactionEntry, error) {
+
+	useCurrencyBalance, err := s.GetBalance(ctx, input.UserID, input.UseCurrencyID)
+	if err != nil {
+		return nil, fmt.Errorf("error getting balance for use currency %w", err)
+	}
+	getCurrencyBalance, err := s.GetBalance(ctx, input.UserID, input.GetCurrencyID)
+	if err != nil {
+		return nil, fmt.Errorf("error getting balance for get currency %w", err)
+	}
+
+	// do we have enough crypto to sell?
+	if useCurrencyBalance.Amount.LessThan(input.Amount) {
+		return nil, errors.New("insufficient funds for transaction")
+	}
+
+	entry, err := s.SqlClient.InsertTransaction(ctx, input)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = s.SqlClient.InsertTransactionSessionMapEntry(ctx, models.TransactionSessionMapEntry{
+		TradeSessionID: tradeSessionID,
+		TransactionID:  input.ID,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	useBalanceUpdate := models.BalanceEntry{
+		UserID:     input.UserID,
+		CurrencyID: input.UseCurrencyID,
+		Amount:     useCurrencyBalance.Amount.Sub(input.Amount),
+	}
+	_, err = s.UpdateBalance(ctx, useBalanceUpdate)
+	if err != nil {
+		return nil, fmt.Errorf("error updating balance %w", err)
+	}
+
+	received := input.Price.Mul(input.Amount)
+	getBalanceUpdate := models.BalanceEntry{
+		UserID:     input.UserID,
+		CurrencyID: input.GetCurrencyID,
+		Amount:     getCurrencyBalance.Amount.Add(received),
+	}
+	_, err = s.UpdateBalance(ctx, getBalanceUpdate)
+	if err != nil {
+		return nil, fmt.Errorf("error updating balance %w", err)
+	}
+
+	return entry, nil
+}
+
+func (s *StorageClient) CreateTransaction(ctx context.Context, tradeSessionID string, input models.TransactionEntry) (*models.TransactionEntry, error) {
+	var entry *models.TransactionEntry
+	var err error
+
+	switch input.TransactionType {
+	case "BUY":
+		entry, err = s.buy(ctx, tradeSessionID, input)
+	case "SELL":
+		entry, err = s.sell(ctx, tradeSessionID, input)
+	default:
+		return nil, fmt.Errorf("invalid transaction type %v", input.TransactionType)
+	}
+	return entry, err
 }
 
 func (s *StorageClient) GetTransaction(ctx context.Context, transactionID, userID string) (*models.TransactionEntry, error) {
